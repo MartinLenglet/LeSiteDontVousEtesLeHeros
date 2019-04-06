2583b3330e21fa6c12bd4c0a61e17a8c79828da3
MartinLenglet <martinlenglet@gmail.com>
2019-04-06 13:47:00 +0200

    ajout carousel

--- front/view/onglet/index.php
+++ front/view/onglet/index.php
@@ -9,10 +9,60 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
     <title>Page d'accueil</title>
 </head>
+
 <body>
     <?php
         include 'menu.php';
     ?>
+
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner">
+
+      <div class="item active img_carousel">
+        <img src="../../images/dragon.jpg" alt="Los Angeles" class="img_carousel">
+        <div class="carousel-caption">
+          <h3>Los Angeles</h3>
+          <p>LA is always so much fun!</p>
+        </div>
+      </div>
+
+      <div class="item img_carousel">
+        <img src="../../images/HEROIC FANTASY B.jpg" alt="Chicago" class="img_carousel">
+        <div class="carousel-caption">
+          <h3>Chicago</h3>
+          <p>Thank you, Chicago!</p>
+        </div>
+      </div>
+    
+      <div class="item img_carousel">
+        <img src="../../images/bandeau-renégats.jpg" alt="New York" class="img_carousel">
+        <div class="carousel-caption">
+          <h3>New York</h3>
+          <p>We love the Big Apple!</p>
+        </div>
+      </div>
+  
+    </div>
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+
     <div class="container">
         <div class="row">
             <div class="news col-lg-12">
