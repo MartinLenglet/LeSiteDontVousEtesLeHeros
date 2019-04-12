3ec97609c6627338d66b52d2c6a035de5ca06827
MartinLenglet <martinlenglet@gmail.com>
2019-04-12 19:39:14 +0200

    choix du perso

--- front/view/onglet/choix_personnage.php
+++ front/view/onglet/choix_personnage.php
@@ -9,31 +9,6 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
     <script scr="../js/choix_personnage.js"></script>
     <title>Choix du personnage</title>
- 
-    <script>
-        $(document).ready(function(){
-            $("#perso1").mouseover(function() {
-                $("#statPerso").show("slow");
-            });
-        });
-        /*$(document).ready(function(){
-            $("#perso1").mouseout(function() {
-                $("#statPerso").hide("slow");
-            });
-        });
-        $(document).ready(function(){
-            $("#progress").mouseover(function() {
-                $("#statPerso").show("slow");
-            });
-        });
-        $(document).ready(function(){
-            $("#btn_suppr").mouseover(function() {
-                $("#statPerso").show("slow");
-            });
-        });*/
-    </script> 
-     
-
 </head>
 <body>
 
@@ -43,53 +18,24 @@
 
     <div class="container">
         <div class="row">
-            <div id="perso1" class="col-lg-5 perso">
-                <div id="statPerso" class="statPerso">
-
-                    <div class="progress">
-                        <div class="progress-bar" role="progressbar" aria-valuenow="40"
-                        aria-valuemin="0" aria-valuemax="100" style="width:40%; background-color:red">
-                            <span class="sr-only">70% Complete</span>
-                            Force
-                        </div> 
-                    </div>
-                    
-                    <div class="progress">
-                        <div class="progress-bar" role="progressbar" aria-valuenow="70"
-                        aria-valuemin="0" aria-valuemax="100" style="width:70%; background-color:green">
-                            <span class="sr-only">70% Complete</span>
-                            Habilité
-                        </div>
-                    </div> 
-                    
-                    
-                    <div class="progress">
-                        <div class="progress-bar" role="progressbar" aria-valuenow="30"
-                        aria-valuemin="0" aria-valuemax="100" style="width:30%; background-color:blue">
-                            <span class="sr-only">70% Complete</span>
-                            Robustesse
-                        </div>
-                    </div> 
-                    
-                    
-                    <div class="progress">
-                        <div class="progress-bar" role="progressbar" aria-valuenow="60"
-                        aria-valuemin="0" aria-valuemax="100" style="width:60%; background-color:purple">
-                            <span class="sr-only">70% Complete</span>
-                            Intelligence
-                        </div>
-                    </div> 
-                    
-                    <div class="textPerso">
-                        <h2>Nom du personnage</h2>
-                    </div>
-
-                    <button type="button" class="btn_suppr btn btn-danger btn-block">Supprimer</button> 
+            <div id="perso" class="col-lg-5 perso">
+                <div class="imgPerso">
+                    <img src="../../images/perso1.jpg">
+                </div>    
+                <div class="textPerso">
+                    <h2>Nom du personnage</h2>
                 </div>
+                <button type="button" class="btnSuppr btn btn-danger btn-block">Supprimer</button> 
             </div>
+
             <div class="col-lg-2">
             </div>
+
             <div class="col-lg-5 perso">
+                <div class="textPerso">
+                    <h2>Création de personnage</h2>
+                </div>
+                <button type="button" class="btnAdd btn btn-primary btn-lg">Création</button>
             </div>
         </div>
 
